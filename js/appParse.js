e98c85fbb825055c7e2911d7fb4a9ea72d4df2dc
Irvollo <irvollo@hotmail.com>
2014-05-26 10:58:28 -0500

    funcion borrar

--- js/appParse.js
+++ js/appParse.js
@@ -3,10 +3,10 @@ var parseRestKey = '8kaYXfz78Bm7VziKTRLTIiOLLpyFGOA7nAdjpa17';
 
 $(document).ready(function() {
 	getMessages();
+	$('.table tbody').on('click', 'td a.linkdeleteuser', deleteMessage);
 	$('#send').click(function() {
 		var username = $('input[name=username]').attr('value');
 		var message = $('input[name=message]').attr('value');
-		alert(username + '!');
 		$.ajax({
 			url: 'https://api.parse.com/1/classes/MessageBoard',
 			headers: {
@@ -60,8 +60,36 @@ function updateView(messages) {
 					+ value.username
 					+ '</td><td>'
 					+ value.message
+					+ '</td><td>'
+					+ '<a class="linkdeleteuser" name="'+ value.objectId +'" rel="'+ value.objectId +'">Delete</a>'
 					+ '</td></tr>');
 		table.append(trEl);
 	});
 	console.log(messages); 
+}
+
+
+function deleteMessage(event) {
+	event.preventDefault();
+	var con = confirm('Are you sure you want to delete this message?');
+	console.log($(this).attr('rel'));
+	if(con === true) {
+		$.ajax({
+			url: 'https://api.parse.com/1/classes/MessageBoard/' + $(this).attr('rel'),
+			headers: {
+				'X-Parse-Application-Id': parseID,
+				'X-Parse-REST-API-Key': parseRestKey
+			},
+			contentType: 'application/json',
+			dataType: 'json',
+			type: 'DELETE',
+			success: function() {
+				console.log('success');
+				getMessages();
+			}, 
+			error: function() {
+				console.log('ERROR!');
+			}
+		});
+	}
 }
